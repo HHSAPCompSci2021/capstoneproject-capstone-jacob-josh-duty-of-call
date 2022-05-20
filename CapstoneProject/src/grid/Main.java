e06c7643d5072eb2a3db4706ca3a8d49190ca9cd
joshlee746 <jlee275@student.fuhsd.org>
2022-05-20 11:59:13 -0700

    Update Main.java

--- CapstoneProject/src/grid/Main.java
+++ CapstoneProject/src/grid/Main.java
@@ -26,7 +26,7 @@ public class Main {
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
 		JFrame window = (JFrame)canvas.getFrame();
 
-		window.setSize(800, 600);
+		window.setSize(900, 700);
 		window.setMinimumSize(new Dimension(100,100));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(true);
