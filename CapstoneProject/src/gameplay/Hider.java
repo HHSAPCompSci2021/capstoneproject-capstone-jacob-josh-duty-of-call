efa290f18a1abdd1d5cdb8144001e299af9a63b1
Jacob Tang <jtang320@student.fuhsd.org>
2022-05-22 23:51:44 -0700

    map build

--- CapstoneProject/src/gameplay/Hider.java
+++ CapstoneProject/src/gameplay/Hider.java
@@ -24,13 +24,19 @@ public class Hider extends Sprite {
 	
 	public static final int HIDER_WIDTH = 30;
 	public static final int HIDER_HEIGHT = 30;
+<<<<<<< Updated upstream
 	
 	private SpecialPowers speedBoost;
 	private boolean wallPhase;
 	private boolean taserStun;
 	private boolean invincible;
+=======
+
+>>>>>>> Stashed changes
 	
 	private Map map;
+	
+	private SpecialPowers powers;
 	
 	private PImage img;
 	
@@ -42,6 +48,7 @@ public class Hider extends Sprite {
 		super(img, x, y, HIDER_WIDTH, HIDER_HEIGHT);
 		this.img = img;
 		lives = 1;
+		powers = null;
 		
 	}
 	
@@ -93,7 +100,9 @@ public class Hider extends Sprite {
 		
 	}
 	
-	
+	public SpecialPowers getPower() {
+		return powers; 
+	}
 	
 	public void accelerate(int dirX, int dirY) {
 		if (dirX == 1) {
@@ -110,6 +119,7 @@ public class Hider extends Sprite {
 		}
 	}
 	
+<<<<<<< Updated upstream
 	public void assignPowers() {
 		
 	
@@ -133,6 +143,8 @@ public class Hider extends Sprite {
 		return false;
 	}
 	
+=======
+>>>>>>> Stashed changes
 	
 	public boolean isTagged(Seeker other) {
 		if(this.intersects(other)) {
